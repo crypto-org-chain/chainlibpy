48987edc4d2845263dd5a13ee7e8ac9f964f33b3
linfeng <linfeng@crypto.com>
2020-11-20 12:30:30 +0800

    remove timeout_heigt from signature

--- cryptopy/_transaction.py
+++ cryptopy/_transaction.py
@@ -6,7 +6,7 @@ from typing import Any, Dict, List
 
 import ecdsa
 
-from cryptopy._wallet import DEFAULT_BECH32_HRP, DEFAULT_BECH32_HRP_BASE, Wallet
+from cryptopy._wallet import Wallet
 
 if sys.version_info < (3, 8):
     from typing_extensions import Literal
@@ -15,9 +15,9 @@ else:
 
 
 # Valid transaction broadcast modes for the `POST /txs` endpoint of the
-# Cosmos REST API.
+# Crypto.com REST API.
 SyncMode = Literal["sync", "async", "block"]
-
+DEFAULT_BECH32_HRP_BASE = "basecro"
 
 class Transaction:
     """A Cosmos transaction.
@@ -39,9 +39,7 @@ class Transaction:
         fee_denom: str = "basecro",
         memo: str = "",
         chain_id: str = "crypto-crocncl-1",
-        hrp: str = DEFAULT_BECH32_HRP,
         sync_mode: SyncMode = "sync",
-        timeout_height: int = 0,
     ) -> None:
         self._wallet = wallet
         self._account_num = account_num
@@ -51,32 +49,26 @@ class Transaction:
         self._gas = gas
         self._memo = memo
         self._chain_id = chain_id
-        self._hrp = hrp
         self._sync_mode = sync_mode
-        self._timeout_height = timeout_height
         self._msgs: List[dict] = []
 
     def add_transfer(
-        self, to_address: str, amount: int, denom: str = DEFAULT_BECH32_HRP_BASE
+        self, to_address: str, amount: int, base_denom: str = DEFAULT_BECH32_HRP_BASE
     ) -> None:
         transfer = {
             "type": "cosmos-sdk/MsgSend",
             "value": {
                 "from_address": self._wallet.address,
                 "to_address": to_address,
-                "amount": [{"denom": denom, "amount": str(amount)}],
+                "amount": [{"denom": base_denom, "amount": str(amount)}],
             },
         }
         self._msgs.append(transfer)
 
-    def get_pushable(self) -> str:
+    def get_pushable(self) -> dict:
         """
-        get the request post to the /txs, https://cosmos.network/rpc/v0.37.9
+        get the request post to the /txs
         """
-        if self._fee == 0:
-            amount = [{"amount": str(self._fee), "denom": self._fee_denom}]
-        else:
-            amount = []
         pubkey = self._wallet.public_key
         base64_pubkey = base64.b64encode(pubkey).decode("utf-8")
         pushable_tx = {
@@ -84,7 +76,7 @@ class Transaction:
                 "msg": self._msgs,
                 "fee": {
                     "gas": str(self._gas),
-                    "amount": amount,
+                    "amount": [{"amount": str(self._fee), "denom": self._fee_denom}],
                 },
                 "memo": self._memo,
                 "signatures": [
@@ -98,13 +90,11 @@ class Transaction:
             },
             "mode": self._sync_mode,
         }
-        return json.dumps(pushable_tx, separators=(",", ":"))
+        return pushable_tx
 
     def _sign(self) -> str:
         message_str = json.dumps(self._get_sign_message(), separators=(",", ":"), sort_keys=True)
-        print(message_str)
         message_bytes = message_str.encode("utf-8")
-        print(message_bytes)
 
         privkey = ecdsa.SigningKey.from_string(self._wallet.private_key, curve=ecdsa.SECP256k1)
         signature_compact = privkey.sign_deterministic(
@@ -115,11 +105,6 @@ class Transaction:
         return signature_base64_str
 
     def _get_sign_message(self) -> Dict[str, Any]:
-        if self._fee == 0:
-            amount = []
-        else:
-            amount = [{"amount": str(self._fee), "denom": self._fee_denom}]
-
         result = {
             "account_number": str(self._account_num),
             "sequence": str(self._sequence),
@@ -127,10 +112,8 @@ class Transaction:
             "memo": self._memo,
             "fee": {
                 "gas": str(self._gas),
-                "amount": amount,
+                "amount": [{"amount": str(self._fee), "denom": self._fee_denom}],
             },
             "msgs": self._msgs,
         }
-        if self._timeout_height != 0:
-            result["timeout_height"] = str(self._timeout_height)
         return result
