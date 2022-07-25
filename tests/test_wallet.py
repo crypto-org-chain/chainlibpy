2ca760286fe34927a797efe71b529ed0b6ca07f7
Calvin Lau <calvin@crypto.com>
2022-07-25 16:32:15 +0800

    Problem: Copyright is missing attribution
    Solution: Update LICENSE file

--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2020, hukkinj1 (licensed under the MIT License)
-# Modifications Copyright (c) 2020-present (licensed under the Apache License, Version 2.0)
+# Modifications Copyright (c) 2020-present Crypto.org (licensed under the Apache License, Version 2.0)
 
 from chainlibpy.wallet import Wallet
 
